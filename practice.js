f90bab6ddfaa7cc4c6fbfeafc0acb325517e102b
kammitama5 <krystal.maughan@gmail.com>
2017-06-04 00:09:38 +0000

    practice.js

---
+++ practice.js
@@ -0,0 +1,23 @@
+// while loop
+var i = 0
+while (i < 6){
+	console.log(i);
+	i++
+}
+
+// for loop
+// var should be in.
+for (var i = 0; i<6; i++){
+	console.log(i)
+}
+
+// function
+function square(x){
+	return x*x;
+}
+
+console.log(square(10));
+console.log(square(1.4));
+
+
+
