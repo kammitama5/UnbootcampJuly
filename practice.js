0d111def9d156db0dbb91fb0c660dea4cbe84e5b
kammitama5 <krystal.maughan@gmail.com>
2017-06-04 00:27:51 +0000

    update to practice.js one

--- practice.js
+++ practice.js
@@ -19,5 +19,24 @@ function square(x){
 console.log(square(10));
 console.log(square(1.4));
 
+function isRainy(rainy, umbrella){
+    
+    if ((rainy == true) && (umbrella == true))
+    {
+        return "You are in Cali!";
+    }
+    else if ((rainy == false) && (umbrella == true))
+    {
+        return "Why'd you bring your umbrella? ";
+    }
+    
+    else
+    {
+        return "It's rainy, fool!";
+    }
+    
+    
+    
+}
 
-
+console.log(isRainy(false, false));
